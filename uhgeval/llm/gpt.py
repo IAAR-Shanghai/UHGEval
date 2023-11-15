0d03c05deea92b7cadd6438005186c665344f767
Ki-Seki <Ki_Seki@outlook.com>
2023-11-15 21:40:31 +0800

    refactor (gpt, remote): change the behavior of token_consumed

--- uhgeval/llm/gpt.py
+++ uhgeval/llm/gpt.py
@@ -13,7 +13,6 @@ class GPT(BaseLLM):
     def __init__(self, model_name='gpt-3.5-turbo', temperature=1.0, max_new_tokens=1024, report=False):
         super().__init__(model_name, temperature, max_new_tokens)
         self.report = report
-        self.token_consumed = 0
 
     def request(self, query: str) -> str:
         openai.api_key = conf.GPT_api_key
@@ -26,6 +25,6 @@ class GPT(BaseLLM):
         )
         real_res = res["choices"][0]["message"]["content"]
 
-        self.token_consumed += res['usage']['total_tokens']
-        logger.info(f'GPT token consumed: {self.token_consumed}') if self.report else ()
+        token_consumed = res['usage']['total_tokens']
+        logger.info(f'GPT token consumed: {token_consumed}') if self.report else ()
         return real_res
