b73844338a7addd00abc4121e03cf8f8d062b2a4
Ki-Seki <Ki_Seki@outlook.com>
2023-12-26 16:17:56 +0800

    refactor: api and remote model configurations

--- uhgeval/configs/example_config.py
+++ uhgeval/configs/example_config.py
@@ -1,3 +1,20 @@
+# ─── Models Accessed Through API ──────────────────────────────────────────────
+# Note: If you want to use the models below, you need to apply for an API key for each model.
+
+# Baichuan 53B
+# Visit: https://platform.baichuan-ai.com/docs/api
+Baichuan2_53B_url = ''
+Baichuan2_53B_api_key = ''
+Baichuan2_53B_secret_key = ''
+
+# GPT series
+# https://platform.openai.com/docs/guides/text-generation
+GPT_api_key = ''
+
+
+# ─── Models Accessed Through Remote Deployment ────────────────────────────────
+# Note: Remote models are only for internal use.
+
 Aquila_url = ''
 Aquila_token = ''
 
@@ -5,14 +22,8 @@ Baichuan2_13B_url = ''
 Baichuan2_13B_token = ''
 
 
-Baichuan2_53B_url = ''
-Baichuan2_53B_api_key = ''
-Baichuan2_53B_secret_key = ''
-
 ChatGLM2_url = ''
 ChatGLM2_token = ''
-
-GPT_api_key = ''
 
 GPT_transit_url = ''
 GPT_transit_token = ''
