98172db45fa5ed6544c25f9dd44e9f362d5a71ca
春晖如画 <57168705+wyzh0912@users.noreply.github.com>
2024-01-14 19:10:05 +0800

    feat: added config.yaml and update run_uhgeval.py

--- run_uhgeval.py
+++ run_uhgeval.py
@@ -1,52 +1,80 @@
+import importlib
 import sys
-from loguru import logger
 
-from uhgeval.dataset.xinhua import XinhuaHallucinations
-from uhgeval.evaluator.discriminative import (
-    DiscriminativeEvaluatorKeywordLevel,
-    DiscriminativeEvaluatorSentenceLevel
-)
-from uhgeval.evaluator.generative import GenerativeEvaluator
-from uhgeval.evaluator.selective import SelectiveEvaluator
+from loguru import logger
 from uhgeval.core.analyst import save_overalls, save_overalls_by_type
 from uhgeval.core.experiment import experiment_in_blocks
-from uhgeval.llm.api import (
-    Baichuan2_53B_Chat,
-    GPT,
-)
-from uhgeval.llm.remote import (
-    Aquila_34B_Chat,
-    Baichuan2_13B_Chat,
-    ChatGLM2_6B_Chat,
-    InternLM_20B_Chat,
-    Xinyu_7B_Chat,
-    Xinyu_70B_Chat,
-    Qwen_14B_Chat,
-    GPT_transit,
-)
-
+from uhgeval.core.experiment import load_yaml_conf
 
 if __name__ == '__main__':
     seed = 22
     enable_log_saving = True
     logger.remove()  # Remove all logger handlers including the stderr logger handler
     logger.add(sys.stderr, level=40)  # Update stderr logger
-    logger.add('logs/uhgeval_{time}.log', level=0) if enable_log_saving else ...
-    # TODO: Currently, loguru does not support log settings above when using the 'spawn' method in multiprocessing.
+    logger.add('logs/uhgeval_{time}.log',
+               level=0) if enable_log_saving else ...
+    # TODO: Currently, loguru does not support log settings above when using
+    # the 'spawn' method in multiprocessing.
+    experiment_configs_path = 'config.yaml'
+    experiment_conf = load_yaml_conf(experiment_configs_path)
+    datasets_configs = experiment_conf.get('dataset')
+    evaluated_dataset_list = []
+    evaluator_list = []
 
-    dataset = XinhuaHallucinations('data/Xinhua/XinhuaHallucinations.json', shuffle=True, seed=seed).load()
-    llms = [
-        GPT(model_name='gpt-3.5-turbo', report=True), 
-        GPT(model_name='gpt-4-0613', report=True), 
-        GPT(model_name='gpt-4-1106-preview', report=True), 
-    ]
-    evaluators = [
-        DiscriminativeEvaluatorKeywordLevel, 
-        DiscriminativeEvaluatorSentenceLevel, 
-        GenerativeEvaluator, 
-        SelectiveEvaluator
-    ]
-    
-    experiment_in_blocks(dataset, llms, evaluators, 3, 170, 0, seed)
-    save_overalls()
-    save_overalls_by_type()
+    # Load dataset configs
+    for dataset_dict in datasets_configs:
+        dataset_name = list(dataset_dict.keys())[0]
+        parameters = list(dataset_dict.values())[0]
+        temp_name = dataset_name.split('.')
+        dataset_module_name = f"uhgeval.dataset.{temp_name[0]}"
+        dataset_module = importlib.import_module(dataset_module_name)
+        dataset_class = getattr(dataset_module, temp_name[1])
+
+        # Load evaluator configs
+        for evaluator_config in parameters['evaluator']:
+            temp_list = evaluator_config.split('.')
+            evaluator_module_name = f"uhgeval.evaluator.{temp_list[0]}"
+            evaluator_module = importlib.import_module(evaluator_module_name)
+            evaluator_class = getattr(evaluator_module, temp_list[1])
+            # Instantiate the corresponding class and add it to the evaluator list.
+            evaluator_list.append(evaluator_class)
+        parameters.pop('evaluator')
+
+        # Instantiate the corresponding class and add it to the dataset list.
+        if parameters is not None:
+            evaluated_dataset_list.append(dataset_class(**parameters))
+        else:
+            evaluated_dataset_list.append(dataset_class())
+
+    # Load llm configs
+    llm_configs = experiment_conf.get('llm')
+    evaluated_llm_list = []
+    for llm_type, llm_list in llm_configs.items():
+        if llm_list is None:
+            continue
+        for llm_dict in llm_list:
+            if llm_dict is None:
+                continue
+            llm_name = list(llm_dict.keys())[0]
+            parameters = list(llm_dict.values())[0]
+            llm_module_name = f"uhgeval.llm.{llm_type}"
+            llm_module = importlib.import_module(llm_module_name)
+            llm_class = getattr(llm_module, llm_name)
+
+            # Instantiate the corresponding class and add it to the llm list.
+            if parameters is not None:
+                evaluated_llm_list.append(llm_class(**parameters))
+            else:
+                evaluated_llm_list.append(llm_class())
+
+    for dataset in evaluated_dataset_list:
+        experiment_in_blocks(
+            dataset,
+            evaluated_llm_list,
+            evaluator_list,
+            3,
+            170,
+            0,
+            seed)
+        save_overalls()
+        # save_overalls_by_type()
